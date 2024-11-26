38e3cddfdf5c25a87fa4a1ee722a5d1935a919bd
Nathan Cantafio <cantafionathan@gmail.com>
2024-11-26 10:45:19 -0800

    better organized the parameters fixed time stepping so we can have smooth solving (k<<1) and interpretable tick marks on plot now prints treatments and response after running the script

--- sir.m
+++ sir.m
@@ -2,6 +2,9 @@
 % Turkyilmazoglu M. An extended epidemic model with vaccination: Weak-immune SIRVI. 
 % Physica A. 2022;598:127429. doi:10.1016/j.physa.2022.127429
 
+%-----MODEL-----%
+
+% SIVQR model
 N = 1000000; % total population
 I0 = 0.01*N; % number of people sick once the health authority notices an issue
 S = N - I0; % total number of susceptible individuals
@@ -10,32 +13,45 @@ V = 0; % total number of vaccinated individuals
 Q = 0; % total number of quarantined individuals
 R = 0; % total number of recovered individuals
 
-% response: max(i)
-% treatments: can vary vaccination rate, isolation_factor
-% another idea (quarantining sympotomatic infecteds? - have prob that
-% infected is asymptomatic or not, then quarantine some portion of those that showing)
-% blocking factors???
+%-----Parameters-----%
 
-base_num_inter = 20; % number of daily interactions if there was no policies
-% treatment we can change:
-isolation_factor = 0; % social isolation policy ranging from zero isolation to total isolation
-daily_interactions = base_num_inter*(1-isolation_factor); % average number of daily interactions a susceptible individual has
-probability_of_spread_unvaccinated = 0.1; % probabilty that an unvaccinated individual gets infected from interacting with an infected individual
-probability_of_spread_vaccinated = 0.01*probability_of_spread_unvaccinated; % probability that a vaccinated indidual gets infected from interacting with an infected individual
+% number of daily interactions if there was no policies
+% proxy for population density
+base_num_inter = 20; 
+
+% social isolation policy ranging from zero isolation to total isolation
+% reduces number of daily interactions
+isolation_factor = 0; 
+
+% average number of daily interactions
+daily_interactions = base_num_inter*(1-isolation_factor); 
+
+% nu = vaccination rate of susceptible individuals
+nu = 0.0; % levels 0, 0.1, 0.2, 0.3
+
+% vaccine effectivness
+vac_eff = 0.01;
+
+% probability of getting infected from interacting w/ infected invdividual
+probability_of_spread_unvaccinated = 0.1; 
+probability_of_spread_vaccinated = vac_eff*probability_of_spread_unvaccinated; 
 
 % beta = transmission rate
 beta_unvac = daily_interactions*probability_of_spread_unvaccinated;
 beta_vac = daily_interactions*probability_of_spread_vaccinated;
 
 % gamma = recovery rate
-gamma = 1/14; % covid % recovery in 2 weeks????
-%gamma = 1/31;
+gamma = 1/14; % covid recovery in 2 weeks
 
 %probability that an infectious individual is sympomatic
-prob_symptomatic = 1; % covid
-% treatment we can change:
-quarantine = 1; % quarantining sympomatic individuals, yes or no
-quarantine_duration = min(14, 1/gamma); % 5 days or recovery
+prob_symptomatic = 0.4; % covid
+
+% quarantining sympomatic individuals
+% should always be set to 1, if no quarantine set quarantine duration to 0
+quarantine = 1; 
+
+qd = 5;
+quarantine_duration = min(qd, 1/gamma); % 5 days or recovery
 % rate at which we take people out of quarantine
 if quarantine_duration == 0
     quarantine_rate = 0;
@@ -43,6 +59,7 @@ if quarantine_duration == 0
 else
     quarantine_rate = 1/quarantine_duration;
 end
+
 %rate of recovery for quarantined individuals:
 if quarantine_rate == 0
     gamma_q = gamma;
@@ -52,9 +69,7 @@ else
     gamma_q = 1/(1/gamma - 1/quarantine_rate);
 end
 
-% treatment we can change:
-% nu = vaccination rate of susceptible individuals
-nu = 0.0; % levels 0, 0.1, 0.2, 0.3
+%-----Solver-----%
 
 % ds/dt = -beta_unvac*s*i - nu*s
 % di/dt beta_unvac*s*i + beta_vac*v*i - quarantine*prob_sympomatic*i + (1-gamma_q)*quarantine_rate*q - gamma*i
@@ -63,7 +78,7 @@ nu = 0.0; % levels 0, 0.1, 0.2, 0.3
 % dr/dt = gamma*i + gamma_q*quarantine_rate*q \\ can be solved for from s + i + v + r = 1
 
 % diff equations with forward euler:
-k = 0.0001; % look at this
+k = 0.001; % lower time step for convergence, set to k = 1 for interpretability
 Tf = 90; % days
 numsteps = ceil(Tf/k);
 k = Tf/numsteps;
@@ -94,6 +109,18 @@ plot(1:numsteps, s, 'b', ...
     1:numsteps, v, 'g', ...
     1:numsteps, q, 'm', ...
     1:numsteps, r, 'k')
+xticks(0:10/k:numsteps)
+xticklabels(0:10:Tf)
 legend('Susceptible', 'Infected', 'Vaccinated', 'Quarantined', 'Recovered')
 
-max(i)
+fprintf('PARAMETERS/TREATMENTS: \n')
+fprintf('average number of daily interactions (population density) = %d \n', base_num_inter);
+fprintf('social isolation ranging from 0 (no isolation), to 1 (max isolation) = %d \n',isolation_factor);
+fprintf('probability of spreading to unvaccinated = %d \n',probability_of_spread_unvaccinated);
+fprintf('rate of recovery = %d days \n', 1/gamma);
+fprintf('rate at which susceptible individuals are vaccinated = %d \n',nu);
+fprintf('vaccine effectivness (how much does it reduce prob(spread)?) = %d \n',vac_eff);
+fprintf('prob_symptomatic = %d \n',prob_symptomatic);
+fprintf('quarantine_duration = %d days \n',quarantine_duration);
+fprintf('RESPONSE: \n')
+fprintf('peak of infected population = %d%% \n', round(max(i)*100));
